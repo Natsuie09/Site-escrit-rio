4c176196002fc310ac4f7b0738471edf6887492e
Natsuie09 <natsuie09@users.noreply.github.com>
2026-05-12 02:35:32 -0300

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { RegionalPage } from './pages/RegionalPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/advogado-em/:slug" element={<RegionalPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
